e8e59351a47bb910c516cffe4afa4edf7ee35b50
aprvx <apurvaygokhale@gmail.com>
2024-04-12 14:15:36 +0530

    test commit

---
+++ base/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+# from .views import SignUpView, LoginView, DashboardView
+from .views import home, submit_notification
+
+
+urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+    path('home/',views.home, name="home" ),
+
+    path('',views.home, name="home" ),
+   path('submit-notification/', submit_notification, name='submit_notification'),
+    path('clubs/explore/<str:pk>/',views.explore,name='explore'),
+
+    path('questionnaire/', views.questionnaire, name='questionnaire'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('satisfaction/', views.satisfaction, name='satisfaction'),
+    path('generate_qrcode/<str:club_name>/', views.generate_qrcode, name='generate_qrcode'),
+    path('attend_club/<str:club_name>/', views.attend_club, name='attend_club'),
+    path('api/record_response/', views.record_response, name='record_response'),
+    path('api/record_club/', views.record_club, name='record_club'),
+    path('clubs/', views.clubs, name='clubs'),
+    #path('send_email_notification/', views.send_email_notification, name='send_email_notification'),
+    #path('send-email/', views.send_email, name='send_email'),
+    ###
+]
+
